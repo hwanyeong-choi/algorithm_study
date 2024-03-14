f2c0559949eb14dbcbe97cd65b979c3194b2bfca
include_hoany <47169718+hwanyeongchoi@users.noreply.github.com>
2024-03-14 14:43:02 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int numberOne = scanner.nextInt();
+        int numberTwo = scanner.nextInt();
+        int addNumber = numberOne + numberTwo;
+        System.out.println(addNumber);
+    }
+}
